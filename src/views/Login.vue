eb1f3409523c27e579dda5cdbfd84d10fd9a9f2a
SoulMinT05 <minhtam5111993@gmail.com>
2024-10-15 15:34:43 +0700

    add UI login and register

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -5,79 +5,102 @@ import * as z from 'zod';
 import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
-import { Checkbox } from '@/components/ui/checkbox'
-import { Label } from '@/components/ui/label'
+import { Checkbox } from '@/components/ui/checkbox';
+import { Label } from '@/components/ui/label';
 import { FormControl, FormField, FormLabel, FormItem } from '@/components/ui/form';
 
-const formSchema = toTypedSchema(z.object({
-  password: z.string().min(8),
-  email: z.string().email(),
-}));
+const formSchema = toTypedSchema(
+    z.object({
+        password: z.string().min(8),
+        email: z.string().email(),
+    }),
+);
 
 const form = useForm({
-  validationSchema: formSchema,
+    validationSchema: formSchema,
 });
 
 const onSubmit = form.handleSubmit((val) => {
-  console.log(val);
+    console.log(val);
 });
 </script>
-
 <template>
-  <main class="h-screen w-screen flex items-center justify-center">
-    <Card class="max-w-[320px] md:max-w-[400px] w-full">
-      <CardHeader>
-        <CardTitle class="text-center">Login</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <form @submit="onSubmit">
-          <FormField v-slot="{ componentField }" name="email">
-            <FormItem class="mb-4">
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="text" placeholder="example@mail.com" v-bind="componentField" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          </FormField>
-          <FormField v-slot="{ componentField }" name="password">
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" v-bind="componentField" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          </FormField>
-          <div class="flex items-center space-x-2 mt-4">
-            <Checkbox id="terms" />
-            <Label for="terms">Remember Me</Label>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter>
-        <div class="w-full">
-          <Button class="w-full" @click="onSubmit">Login</Button>
-          <div class="relative my-4">
-            <div class="absolute inset-0 flex items-center">
-              <span class="w-full border-t" />
+    <section class="bg-gray-50 dark:bg-gray-900">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <div
+                class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-sm lg:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+            >
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1
+                        class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+                    >
+                        Login to your account
+                    </h1>
+                    <form class="space-y-4 md:space-y-6" action="#">
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >Your email</label
+                            >
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="name@company.com"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >Password</label
+                            >
+                            <input
+                                type="password"
+                                name="password"
+                                id="password"
+                                placeholder="••••••••"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                required
+                            />
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-start">
+                                <div class="flex items-center h-5">
+                                    <input
+                                        id="remember"
+                                        aria-describedby="remember"
+                                        type="checkbox"
+                                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                    />
+                                </div>
+                                <div class="ml-3 text-sm">
+                                    <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
+                                </div>
+                            </div>
+                            <a
+                                href="#"
+                                class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                >Forgot password?</a
+                            >
+                        </div>
+                        <button
+                            type="submit"
+                            class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        >
+                            Sign in
+                        </button>
+                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                            Don’t have an account yet?
+                            <router-link
+                                to="/register"
+                                class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                            >
+                                Sign up here
+                            </router-link>
+                        </p>
+                    </form>
+                </div>
             </div>
-            <div class="relative flex justify-center text-xs uppercase">
-              <span class="bg-background px-2 text-muted-foreground">
-                Or continue with
-              </span>
-            </div>
-          </div>
-          <div class="grid gap-4 grid-cols-2">
-            <Button variant="outline" prepend-icon="GithubIcon" class="w-full">
-              Github
-            </Button>
-            <Button variant="outline" prepend-icon="Gitlab" class="w-full">
-              Gitlab
-            </Button>
-          </div>
         </div>
-      </CardFooter>
-    </Card>
-  </main>
+    </section>
 </template>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -24,56 +24,122 @@ const onSubmit = form.handleSubmit((val) => {
     console.log(val);
 });
 </script>
-
 <template>
     <main class="h-screen w-screen flex items-center justify-center">
-        <Card class="max-w-[320px] md:max-w-[400px] w-full">
-            <CardHeader>
-                <CardTitle class="text-center">Register</CardTitle>
-            </CardHeader>
-            <CardContent>
-                <form @submit="onSubmit">
-                    <FormField v-slot="{ componentField }" name="email">
-                        <FormItem class="mb-4">
-                            <FormLabel>Email</FormLabel>
-                            <FormControl>
-                                <Input type="text" placeholder="example@mail.com" v-bind="componentField" />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    </FormField>
-                    <FormField v-slot="{ componentField }" name="password">
-                        <FormItem>
-                            <FormLabel>Password</FormLabel>
-                            <FormControl>
-                                <Input type="password" v-bind="componentField" />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
-                    </FormField>
-                    <div class="flex items-center space-x-2 mt-4">
-                        <Checkbox id="terms" />
-                        <Label for="terms">Remember Me</Label>
-                    </div>
-                </form>
-            </CardContent>
-            <CardFooter>
-                <div class="w-full">
-                    <Button class="w-full" @click="onSubmit">Login</Button>
-                    <div class="relative my-4">
-                        <div class="absolute inset-0 flex items-center">
-                            <span class="w-full border-t" />
-                        </div>
-                        <div class="relative flex justify-center text-xs uppercase">
-                            <span class="bg-background px-2 text-muted-foreground"> Or continue with </span>
-                        </div>
-                    </div>
-                    <div class="grid gap-4 grid-cols-2">
-                        <Button variant="outline" prepend-icon="GithubIcon" class="w-full"> Github </Button>
-                        <Button variant="outline" prepend-icon="Gitlab" class="w-full"> Gitlab </Button>
+        <section class="bg-gray-50 dark:bg-gray-900">
+            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div
+                    class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-sm lg:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+                >
+                    <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1
+                            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+                        >
+                            Sign up
+                        </h1>
+                        <form class="space-y-4 md:space-y-6" action="#">
+                            <div class="flex gap-4">
+                                <div class="w-1/2">
+                                    <label
+                                        for="firstName"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                        >First Name</label
+                                    >
+                                    <input
+                                        type="text"
+                                        name="firstName"
+                                        id="firstName"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required
+                                    />
+                                </div>
+                                <div class="w-1/2">
+                                    <label
+                                        for="lastName"
+                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                        >Last Name</label
+                                    >
+                                    <input
+                                        type="text"
+                                        name="lastName"
+                                        id="lastName"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        required
+                                    />
+                                </div>
+                            </div>
+                            <div>
+                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    >Your email</label
+                                >
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="name@company.com"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    for="password"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    >Password</label
+                                >
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="••••••••"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required
+                                />
+                            </div>
+
+                            <!-- Uncomment this section if you want to include terms acceptance -->
+                            <!--
+                          <div class="flex items-start">
+                              <div class="flex items-center h-5">
+                                  <input
+                                      id="terms"
+                                      aria-describedby="terms"
+                                      type="checkbox"
+                                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                      required
+                                  />
+                              </div>
+                              <div class="ml-3 text-sm">
+                                  <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the
+                                      <a class="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a>
+                                  </label>
+                              </div>
+                          </div>
+                          -->
+
+                            <button
+                                type="submit"
+                                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            >
+                                Create account
+                            </button>
+                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                                Already have an account?
+                                <router-link
+                                    to="/login"
+                                    class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                >
+                                    Login here
+                                </router-link>
+                            </p>
+                        </form>
                     </div>
                 </div>
-            </CardFooter>
-        </Card>
+            </div>
+        </section>
     </main>
 </template>
+
+<style scoped>
+/* Điều chỉnh kích thước form */
+</style>
